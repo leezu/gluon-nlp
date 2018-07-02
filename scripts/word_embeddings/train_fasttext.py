a4923e69bcc8181e0675b1de4ec2265e48bdb418
Leonard Lausen <leonard@lausen.nl>
2018-07-02 20:04:58 +0000

    Add HighwayCNNEmbeddingModel

--- scripts/word_embeddings/train_fasttext.py
+++ scripts/word_embeddings/train_fasttext.py
@@ -102,6 +102,7 @@ def parse_args():
     group = parser.add_argument_group('Model arguments')
     group.add_argument('--emsize', type=int, default=300,
                        help='Size of embedding vectors.')
+    group.add_argument('--subword-network', type=str, default='fasttext')
     group.add_argument('--ngrams', type=int, nargs='+', default=[3, 4, 5, 6])
     group.add_argument(
         '--ngram-buckets', type=int, default=2000000,
@@ -190,7 +191,7 @@ def get_train_data(args):
         for count in idx_to_counts
     ]
 
-    if args.ngram_buckets:
+    if args.subword_network.lower() == 'fasttext':
         with print_time('prepare subwords'):
             subword_function = nlp.vocab.create_subword_function(
                 'NGramHashes', ngrams=args.ngrams,
@@ -211,6 +212,19 @@ def get_train_data(args):
         return (data, negatives_sampler, vocab, subword_function,
                 get_subwords_masks, idx_to_pdiscard, sum_counts,
                 idx_to_subwordidxs)
+    elif args.subword_network.lower() == 'highwaycnn':
+        with print_time('prepare characters / byte mapping'):
+            subword_function = nlp.vocab.create_subword_function(
+                'ByteSubwords')
+
+            # Store subword indices for all words in vocabulary
+            idx_to_subwordidxs = list(subword_function(vocab.idx_to_token))
+            get_subwords_masks = get_subwords_masks_factory(idx_to_subwordidxs)
+            max_subwordidxs_len = max(len(s) for s in idx_to_subwordidxs)
+
+        return (data, negatives_sampler, vocab, subword_function,
+                get_subwords_masks, idx_to_pdiscard, sum_counts,
+                idx_to_subwordidxs)
     else:
         return data, negatives_sampler, vocab, idx_to_pdiscard, sum_counts
 
@@ -220,17 +234,18 @@ def get_subwords_masks_factory(idx_to_subwordidxs):
         np.array(i, dtype=np.int_) for i in idx_to_subwordidxs
     ]
 
-    def get_subwords_masks(indices):
+    def get_subwords_masks(indices, minlength=1):
         subwords = [idx_to_subwordidxs[i] for i in indices]
-        return _get_subwords_masks(subwords)
+        return _get_subwords_masks(subwords, minlength)
 
     return get_subwords_masks
 
 
 @numba_njit
-def _get_subwords_masks(subwords):
+def _get_subwords_masks(subwords, minlength):
     lengths = np.array([len(s) for s in subwords])
     length = np.max(lengths)
+    length = np.max(np.array([length, minlength]))
     subwords_arr = np.zeros((len(subwords), length))
     mask = np.zeros((len(subwords), length))
     for i in numba_prange(len(subwords)):
@@ -256,7 +271,7 @@ def save_params(args, embedding, embedding_out):
 ###############################################################################
 def train(args):
     """Training helper."""
-    if args.ngram_buckets:
+    if args.subword_network.lower() == 'fasttext':
         data, negatives_sampler, vocab, subword_function, \
             get_subwords_masks, idx_to_pdiscard, num_tokens, \
             idx_to_subwordidxs = get_train_data(args)
@@ -265,6 +280,18 @@ def train(args):
             subword_function=subword_function,
             embedding_size=args.emsize,
             weight_initializer=mx.init.Uniform(scale=1 / args.emsize),
+            sparse_grad=not args.no_sparse_grad,
+        )
+    elif args.subword_network.lower() == 'highwaycnn':
+        import blocks
+        data, negatives_sampler, vocab, subword_function, \
+            get_subwords_masks, idx_to_pdiscard, num_tokens, \
+            idx_to_subwordidxs = get_train_data(args)
+        embedding = blocks.HighwayCNNEmbeddingModel(
+            token_to_idx=vocab.token_to_idx,
+            subword_function=subword_function,
+            embedding_size=args.emsize,
+            character_embedding_size=15,
             sparse_grad=not args.no_sparse_grad,
         )
     else:
@@ -296,10 +323,21 @@ def train(args):
     trainer_emb_out = trainer.get_embedding_out_trainer(
         args, embedding_out.collect_params())
 
-    if args.ngram_buckets:
+    if args.subword_network.lower() == 'fasttext':
         trainer_subwords = trainer.get_subword_trainer(
             args, embedding.subword_embedding.collect_params(),
             len(subword_function))
+    elif args.subword_network.lower() == 'highwaycnn':
+        trainer_subwords = trainer.get_subword_trainer(
+            args,
+            list(embedding.cnn.collect_params().values()) +
+            list(embedding.character_embedding.collect_params().values()),
+            len(subword_function))
+
+    if args.subword_network.lower() in ['fasttext', 'highwaycnn']:
+        minlength = 1
+        if hasattr(embedding, 'subwordminlength'):
+            minlength = embedding.subwordminlength
 
     def skipgram_batch(data):
         """Create a batch for Skipgram training objective."""
@@ -313,7 +351,7 @@ def train(args):
         masks = mx.nd.concat(word_context_mask, negatives_mask, dim=1)
         labels = mx.nd.concat(word_context_mask, mx.nd.zeros_like(negatives),
                               dim=1)
-        if not args.ngram_buckets:
+        if args.subword_network.lower() not in ['fasttext', 'highwaycnn']:
             return (centers.as_in_context(context[0]),
                     context_negatives.as_in_context(context[0]),
                     masks.as_in_context(context[0]),
@@ -323,7 +361,8 @@ def train(args):
                                                        return_inverse=True)
             inverse_unique_indices = mx.nd.array(inverse_unique_indices,
                                                  ctx=context[0])
-            subwords, subwords_mask = get_subwords_masks(unique.astype(int))
+            subwords, subwords_mask = get_subwords_masks(
+                unique.astype(int), minlength)
 
             return (centers.as_in_context(context[0]),
                     context_negatives.as_in_context(context[0]),
@@ -345,7 +384,7 @@ def train(args):
             mx.nd.ones_like(centers), negatives_mask, dim=1)
         labels = mx.nd.concat(
             mx.nd.ones_like(centers), mx.nd.zeros_like(negatives), dim=1)
-        if not args.ngram_buckets:
+        if args.subword_network.lower() not in ['fasttext', 'highwaycnn']:
             return (word_context.as_in_context(context[0]),
                     word_context_mask.as_in_context(context[0]),
                     center_negatives.as_in_context(context[0]),
@@ -356,7 +395,8 @@ def train(args):
                                                        return_inverse=True)
             inverse_unique_indices = mx.nd.array(inverse_unique_indices,
                                                  ctx=context[0])
-            subwords, subwords_mask = get_subwords_masks(unique.astype(int))
+            subwords, subwords_mask = get_subwords_masks(
+                unique.astype(int), minlength)
             return (word_context.as_in_context(context[0]),
                     word_context_mask.as_in_context(context[0]),
                     center_negatives.as_in_context(context[0]),
@@ -472,15 +512,17 @@ def train(args):
 
             loss.backward()
             num_update += len(label)
+
             if 'adagrad' not in args.optimizer.lower():
                 trainer_emb_in.set_learning_rate(
                     max(0.0001, args.lr * (1 - progress)))
                 trainer_emb_out.set_learning_rate(
                     max(0.0001, args.lr * (1 - progress)))
 
-            # TODO change when adding CNN
-            if ('adagrad' not in args.subword_sparse_optimizer.lower()
-                    and args.ngram_buckets):
+            if (args.subword_network.lower() == 'fasttext'
+                    and 'adagrad' not in args.subword_sparse_optimizer.lower()
+                ) or (args.subword_network.lower() == 'highwaycnn' and
+                      'adagrad' not in args.subword_dense_optimizer.lower()):
                 trainer_subwords.set_learning_rate(
                     max(0.0001, args.subword_sparse_lr * (1 - progress)))
 
@@ -488,13 +530,13 @@ def train(args):
                 (args.eval_interval and ((i + 1) % args.eval_interval == 0))):
                 if 'proximal' in args.optimizer:
                     trainer_emb_in._optimizer.lazy_update = False
-                if (args.ngram_buckets
+                if (args.subword_network.lower() == 'fasttext'
                         and 'proximal' in args.subword_sparse_optimizer):
                     trainer_subwords._optimizer.lazy_update = False
             trainer_emb_in.step(batch_size=1)
             trainer_emb_out.step(batch_size=1)
             trainer_emb_in._optimizer.lazy_update = True
-            if args.ngram_buckets:
+            if args.subword_network.lower() in ['fasttext', 'highwaycnn']:
                 trainer_subwords.step(batch_size=1)
                 trainer_subwords._optimizer.lazy_update = True
 
@@ -534,7 +576,7 @@ def train(args):
                     word_vector_norm_max=vector_norm.max().asscalar(),
                 )
 
-                if args.ngram_buckets:
+                if args.subword_network.lower() == 'fasttext':
                     subword_idx_to_vec = embedding.subword_embedding.embedding.weight.data(
                         ctx=context[0]).as_in_context(
                             mx.cpu()).tostype('default')
@@ -591,8 +633,9 @@ def evaluate(args, embedding, vocab, global_step, eval_analogy=False):
         if not args.no_eval_analogy:
             eval_tokens_set.update(vocab.idx_to_token)
 
-        if not args.ngram_buckets:
+        if args.subword_network.lower() not in ['fasttext', 'highwaycnn']:
             # Word2Vec does not support computing vectors for OOV words
+            # TODO replace with __contains__ check
             eval_tokens_set = filter(lambda t: t in vocab, eval_tokens_set)
 
         eval_tokens = list(eval_tokens_set)
